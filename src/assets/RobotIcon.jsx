d80e67c123990ada5754ae380dbff2b7dc565569
parth-sanrusha <Parth.Gopani@sanrusha.com>
2026-01-07 16:54:29 +0530

    Updates

--- src/assets/RobotIcon.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function RobotIcon() {
-  return <div></div>;
-}
-
-export default RobotIcon;
--- src/components/About.jsx
+++
@@ -1,9 +0,0 @@
-import { BiAlignRight } from "react-icons/bi";
-const About = () => {
-  return (
-    <button>
-      <BiAlignRight />
-    </button>
-  );
-};
-export default About;
